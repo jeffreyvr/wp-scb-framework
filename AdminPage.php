1ce0098d769b0e7780afc7adeca110b4012bc8f4
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2022-02-28 21:02:15 +0100

    Comment out contextual help

--- AdminPage.php
+++ AdminPage.php
@@ -135,7 +135,9 @@ abstract class scbAdminPage {
 		}
 
 		add_action( 'admin_menu', array( $this, 'page_init' ), $this->args['admin_action_priority'] );
-		add_filter( 'contextual_help', array( $this, '_contextual_help' ), 10, 2 );
+
+		// Commented out due to deprecated notice.
+		// add_filter( 'contextual_help', array( $this, '_contextual_help' ), 10, 2 );
 
 		if ( $file ) {
 			$this->file = $file;
